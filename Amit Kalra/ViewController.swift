ec3e4a39ee258d555d9ffea7e01c7c6f19231c41
Amit Nivedan Kalra <amitnkalra@icloud.com>
2016-04-18 18:57:28 -0700

    Initial Commit

---
+++ Amit Kalra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amit Kalra
+//
+//  Created by Amit Kalra on 4/18/16.
+//  Copyright © 2016 Amit Nivedan Kalra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
